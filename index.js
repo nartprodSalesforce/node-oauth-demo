0a4c85454a227b8c667b47304bb3846f6f293ec5
Stuart Werner <swerner@salesforce.com>
2018-05-12 05:36:50 -0400

    Please enter the commit message for your changes. Lines starting

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const hbs = require('hbs'); //handbars backage for formating templates on the page
 const fs = require('fs');
-const port=  process.env.PORT || 3000;  //dynamic port assignment
+const port=  process.env.PORT || 5000;  //dynamic port assignment
+var redirect;
+var  client_id;
 var querystring = require('querystring');
 var https = require('https');
 
@@ -39,21 +41,80 @@ app.get('/',(req,res)=> {
 
 });
 app.get('/oauth',(req,res)=>{
-    var sforg = req.query.sforg; 
-    var secret = req.query.secret; 
-    var redirect = req.query.redirect; 
-    
+    const sforg = req.query.sforg; 
+    client_id = req.query.client_id; 
+    redirect = req.query.redirect; 
+    const url = 'https://'  + sforg + '/services/oauth2/authorize?response_type=code&client_id=' + client_id + '&redirect_uri=http://' + redirect + '&state=mystate2';
+
     console.log('salesforce org: ' +  sforg);
-    console.log('consumer secret ' +  secret);
+    console.log('consumer client_id ' +  client_id);
     console.log('redirect ' +  redirect);
+    console.log('url ' +  url);
     //
+    /*
     res.render('oauth.hbs',{
         pageTitle: 'OAuth Page'
 
-    });
-    
+    });*/
+    res.redirect(url);
 });
+app.get('/abc2',function (req, res){
+  console.log('in /abc2');
+  console.log('Back From salesforce connected app in /abc2');
+  //req is the URL . we need to get the token from req and  create url in res to send to salesforce
+  //  process response 
+  console.log(req.url);
+  //var access_token = (req.url.split("&")[0]).split("=")[1];
+  var access_token = req.query.code;
+  var req_state = req.query.state;
+  console.log('access_token ' + access_token);
+  console.log('req_state ' + req_state);
+  var post_data = querystring.stringify({
+    'grant_type': 'authorization_code',
+    'client_id': client_id,
+    'client_secret': '7730318269731011462',
+    'redirect_uri': 'http://'+ redirect,
+    'code': access_token
+  });
+  var post_options = {
+    host: 'login.salesforce.com',
+    port: '443',
+    path: '/services/oauth2/token',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': Buffer.byteLength(post_data)
+    }
+  };
+  var post_req = https.request(post_options, function (res2) {
+    res2.setEncoding('utf8');
+    res2.on('data', function (chunk) {
 
+      console.log('Response: ' + chunk);
+      var parsed = JSON.parse(chunk);
+      console.log('id: ' + parsed.id);
+      console.log('issued_at: ' + parsed.issued_at);
+      console.log('instance_url: ' + parsed.instance_url);
+      console.log('signature: ' + parsed.signature);
+      console.log('issued_at: ' + parsed.issued_at);
+      console.log('access_token: ' + parsed.access_token);
+      console.log('token_type: ' + parsed.token_type);
+      console.log('scope: ' + parsed.scope);
+      
+      res.end('we are done. parsed response id = ' + parsed.id + '  ' +
+                'issued_at = ' + parsed.issued_at + ' ' + 
+                'instance_url = ' + parsed.instance_url + ' ' + 
+                'signature = ' + parsed.signature + ' ' + 
+                'access_token = ' + parsed.access_token + ' ' + 
+                'token_type = ' + parsed.token_type + ' ' + 
+                'scope = ' + parsed.scope + ' ');
+    });
+
+  });
+  post_req.write(post_data);
+  post_req.end();
+  console.log('/abc2 at end');
+});
 
 //Listen for request on the port
 app.listen(port,()=>{
